57d3bfe478abafeb248b13f93873117b92a7464b
Héctor Arroyo <churruka@hangar18.cc>
2014-05-22 01:11:40 +0200

    Remove script output to stdout

--- gmetric_xfs.sh
+++ gmetric_xfs.sh
@@ -6,7 +6,7 @@ for dev in `awk '$3=="xfs" { print $1 }' /proc/mounts`
 do
         FRAG=$(xfs_db -r -c frag $dev | awk '{print $8}' | cut -f1 -d"." | sed 's/,/\./' | sed 's/%//' )
         dev=`basename $dev`
-        echo "${dev}_frag: $FRAG"
+        // echo "${dev}_frag: $FRAG"
         $GMETRIC --type=float --name="${dev}_frag" --units="%" --value="${FRAG}" --group="fragmentation"
 done
 
