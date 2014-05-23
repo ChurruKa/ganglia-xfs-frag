8398ec5dc23cfba57df01e949a356ab6589f9055
Héctor Arroyo <churruka@hangar18.cc>
2014-05-23 08:07:47 +0200

    Add full path to xfs_db

--- gmetric_xfs.sh
+++ gmetric_xfs.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 GMETRIC=/usr/bin/gmetric
- 
+XFSDB_BIN=/usr/sbin/xfs_db
+
 for dev in `awk '$3=="xfs" { print $1 }' /proc/mounts`
 do
-        FRAG=$(xfs_db -r -c frag $dev | awk '{print $8}' | cut -f1 -d"." | sed 's/,/\./' | sed 's/%//' )
+        FRAG=$(${XFSDB_BIN} -r -c frag $dev | awk '{print $8}' | cut -f1 -d"." | sed 's/,/\./' | sed 's/%//' )
         dev=`basename $dev`
         # echo "${dev}_frag: $FRAG"
         $GMETRIC --type=float --name="${dev}_frag" --units="%" --value="${FRAG}" --group="fragmentation"
